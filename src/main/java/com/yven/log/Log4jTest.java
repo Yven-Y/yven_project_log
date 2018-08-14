a65bf8b12fe3ba79e6d8423115a18e8c2c84e507
yangwen <yven_1@163.com>
2018-08-14 19:27:35 +0800

    test

--- src/main/java/com/yven/log/Log4jTest.java
+++ src/main/java/com/yven/log/Log4jTest.java
@@ -16,72 +16,9 @@ public class Log4jTest {
     public static void main(String[] args) throws InterruptedException {
 
         Log4jTest test = new Log4jTest();
-        test.runThread();
-    }
 
-    public void runThread(){
-        for (int i = 0; i < 500; i++) {
-            new Thread(new ThreadA()).start();
-            new Thread(new ThreadB()).start();
-            new Thread(new ThreadC()).start();
-            new Thread(new ThreadD()).start();
-        }
     }
 
 
-    class ThreadA implements Runnable{
-        @Override
-        public void run() {
-            while(true){
-                log.error(Thread.currentThread().getName());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    class ThreadB implements Runnable{
-        @Override
-        public void run() {
-            while(true){
-                log.warn(Thread.currentThread().getName());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    class ThreadC implements Runnable{
-        @Override
-        public void run() {
-            while (true) {
-                for (int i = 1000; i < 9999; i++) {
-                    log.info("info from=\"" + i + "\" to=\"" + (i + 5) + "\"");
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-    }
-    class ThreadD implements Runnable{
-        @Override
-        public void run() {
-            while(true){
-                log.debug(Thread.currentThread().getName());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }        }
-    }
 }
 
